9ddc2c49d36ef0355f90b7534a32639caf6ac1c0
Gosia <emeneczka@gmail.com>
2017-05-21 16:22:14 +0200

    add properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
-function Telefon(marka, cena, kolor) {
+function Telefon(marka, cena, kolor, pojemnosc) {
 	this.marka = marka;
 	this.cena = cena;
 	this.kolor = kolor;
+	this.pojemnosc = pojemnosc;
 };
 
 Telefon.prototype.printInfo = function() {
-	console.log("Marka telefonu to "+ this.marka +", kolor to "+ this.kolor +", a cena to "+ this.cena +".");
+	console.log("Marka telefonu to "+ this.marka +", kolor to "+ this.kolor +", pojemność to "+ this.pojemnosc +", a cena to "+ this.cena +".");
 };
 
-var SamsungGalaxyS6 = new Telefon("Samsung", 2400, "biały");
+var SamsungGalaxyS6 = new Telefon("Samsung", 2400, "biały", "64GB");
 
-var iPhone6s = new Telefon("Apple", 2900, "srebrny");
-var Huaweip8 = new Telefon("Huawei", 1700, "czarny");
+var iPhone6s = new Telefon("Apple", 2900, "srebrny", "128GB");
+var Huaweip8 = new Telefon("Huawei", 1700, "czarny", "64GB");
 
 
 iPhone6s.printInfo();
